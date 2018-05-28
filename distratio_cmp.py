ffabac727fc853d20a94fef09ce66d225c57fe49
Subhaneil Lahiri <subhylahiri@gmail.com>
2018-05-27 20:08:15 -0700

    AOT compilation with numba

---
+++ distratio_cmp.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 18:28:26 2018
+
+@author: Subhy
+
+distratio
+=========
+Max and min ratios of cross/pair-wise distances squared.
+"""
+import os.path as osp
+import numpy as np
+from numpy.linalg import norm
+import numba as nb
+
+# =============================================================================
+# Compiler class
+# =============================================================================
+
+
+dr_mod = nb.pycc.CC('distratio')
+dr_mod.output_dir = osp.join(dr_mod.output_dir, 'RandProjRandMan', 'MfldProj')
+
+# =============================================================================
+# functions
+# =============================================================================
+
+
+@dr_mod.export('_norm', 'f8(f8[:])')
+def _norm(vec: np.ndarray) -> float:
+    """Squared norm of a vector.
+
+    Parameters
+    -----------
+    vec: ndarray
+        A vector.
+
+    Returns
+    -------
+    nrm: double
+        Euclidean 2-norm squared.
+    """
+    return np.sum(vec**2)
+
+
+@dr_mod.export('pdist_ratio', '(f8,f8)(f8[:,:],f8[:,:])')
+def pdist_ratio(X: np.ndarray, P: np.ndarray) -> (float, float):
+    """Max and min ratio of pair-wise distances squared beween corresponding
+    pairs of points in two sets.
+
+    Parameters
+    -----------
+    X: ndarray
+        Set of points between which we compute pairwise distances for the
+        denominator. Each point is a row.
+    P: ndarray
+        Set of points between which we compute pairwise distances for the
+        numerator.
+
+    Returns
+    -------
+    drmax: double
+        Maximum ratio of distances squared.
+    drmin: double
+        Minimum ratio of distances squared.
+    """
+    drmax = 0.
+    drmin = np.Inf
+    for i in range(X.shape[0]):
+        for j in range(i):
+            ratio = norm(P[i] - P[j]) / norm(X[i] - X[j])
+            if ratio > drmax:
+                drmax = ratio
+            if ratio < drmin:
+                drmin = ratio
+    return drmax, drmin
+
+
+@dr_mod.export('cdist_ratio', '(f8,f8)(f8[:,:],f8[:,:],f8[:,:],f8[:,:]s)')
+def cdist_ratio(XA: np.ndarray, XB: np.ndarray,
+                PA: np.ndarray, PB: np.ndarray) -> (float, float):
+    """Max and min ratio of cross-wise distances squared beween corresponding
+    pairs of points in two groups of two sets.
+
+    Parameters
+    -----------
+    XA: ndarray
+        Set of points *from* which we compute pairwise distances for the
+        denominator. Each point is a row.
+    XB: ndarray
+        Set of points *to* which we compute pairwise distances for the
+        denominator.
+    PA: ndarray
+        Set of points *from* which we compute pairwise distances for the
+        numerator.
+    PB: ndarray
+        Set of points *to* which we compute pairwise distances for the
+        numerator.
+
+    Returns
+    -------
+    drmax: double
+        Maximum ratio of distances squared.
+    drmin: double
+        Minimum ratio of distances squared.
+    """
+    drmax = 0.
+    drmin = np.Inf
+    for i in range(XA.shape[0]):
+        for j in range(XB.shape[0]):
+            ratio = norm(PA[i] - PB[j]) / norm(XA[i] - XB[j])
+            if ratio > drmax:
+                drmax = ratio
+            if ratio < drmin:
+                drmin = ratio
+    return drmax, drmin
+
+
+# =============================================================================
+# Compile it
+# =============================================================================
+
+
+if __name__ == "__main__":
+    dr_mod.compile()
